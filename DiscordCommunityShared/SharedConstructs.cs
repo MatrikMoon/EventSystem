21f99619a1122fccbc81b60c20d55f8eaaf29083
Moon <q@q>
2019-01-13 13:25:24 -0600

    Added Pop Stars

--- DiscordCommunityShared/SharedConstructs.cs
+++ DiscordCommunityShared/SharedConstructs.cs
@@ -8,8 +8,8 @@ namespace DiscordCommunityShared
     public static class SharedConstructs
     {
         public static string Name => "DiscordCommunityPlugin";
-        public static string Version => "0.2.2";
-        public static int VersionCode => 022;
+        public static string Version => "0.2.3";
+        public static int VersionCode => 023;
         public static string Changelog =
             "0.0.1: First Commit. Base Discord/Plugin/Server functional, some commands. Just added \"mode\' differentiation between songs.\n" +
             "0.0.2: Added Oculus support. Added Leaderboard views.\n" +
@@ -30,7 +30,8 @@ namespace DiscordCommunityShared
             "0.1.7: Ninja update II! Fixed Bad Company / songs with improper directory structure\n" +
             "0.2.0: Updated for 0.12.0!\n" +
             "0.2.1: Fixed various issues from previous version (reload score after song, fixed restart button, re-added scoresaber submission)\n" +
-            "0.2.2: Added One Hope from 0.12.1!\n";
+            "0.2.2: Added One Hope from 0.12.1!\n" +
+            "0.2.3: Added Pop Stars from 0.12.2!\n";
 
         public enum Rank
         {
