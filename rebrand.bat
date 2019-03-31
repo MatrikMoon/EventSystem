92a7c06dc570dbf43045f1bae3267cf742a71fdf
Moon <q@q>
2019-03-31 16:18:05 -0500

    Attempt at auto-rebranding

---
+++ rebrand.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+call :find-files %1 %2
+EXIT /B 0
+
+:find-files
+    set PATHS=
+    set NAMES=
+    for /r "%~dp0" %%P in ("*.cs") do (
+        set PATHS=!PATHS! "%%~fP"
+        set NAMES=!NAMES! "%%~nP%%~xP"
+		
+		echo REPLACING IN "%%~fP"
+		call :replace-in-file %~1 %~2 "%%~fP"
+    )
+EXIT /B 0
+
+:replace-in-file
+	set search=%~1
+    set replace=%~2
+    set textFile=%~3
+	set tmpFile=tmp
+	
+	for /f "delims=" %%i in ('findstr /n "^" "%textFile%"') do (
+		set "line=%%i"
+		setlocal enabledelayedexpansion
+		set "line=!line:*:=!"
+		if ["!line!"]==[""] (
+			>>"%tmpFile%" echo(!line!
+		) else (
+			>>"%tmpFile%" echo(!line:%search%=%replace%!
+		)
+		endlocal
+	)
+		
+	break>"%textFile%"
+	
+	type "%tmpFile%" >> "%textFile%"
+	DEL "%tmpFile%"
+EXIT /B 0
